1aefcd68901478addb2dbf0f1e8eed35fb0722c9
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2015-11-17 20:32:06 +0100

    Add pager markup

--- assets/javascript/productTable.js
+++ assets/javascript/productTable.js
@@ -1,39 +1,74 @@
+function expand(start, end){
+    var result = [];
+    while(start<=end){
+        result.push(start);
+        start++;
+    }
+    return result;
+}
+
 var ProductRow = React.createClass({
    render: function(){
        var name = this.props.product.name;
        var price = this.props.product.price;
        return (
-           <tr>
-               <td>{name}</td>
-               <td>€{price}</td>
+           <tr className="productRow">
+               <td className="productName">{name}</td>
+               <td className="productPrice">€{price}</td>
            </tr>
        )
    }
+});
+
+var Pager = React.createClass({
+    render: function(){
+        return (
+            <div className="pager">
+                <a className="pagerPageNumber"> &lt; </a>
+                {
+                expand(1, this.props.numberOfPages)
+                    .map(function(pageNumber){
+                        return <a className="pagerPageNumber"
+                                  key={"pagerPageId"+pageNumber}>
+                            {pageNumber}
+                        </a>
+                    })
+            }
+                    <a className="pagerPageNumber"> &gt; </a>
+            </div>
+        )
+    }
 });
 
 var ProductTable = React.createClass({
     productMatchesSearch: function(product){
         return product.name.toLowerCase().indexOf(this.props.filterText.toLowerCase())!=-1;
     },
+    calculateAmountOfPages: function(){
+        return Math.ceil(this.props.products.length/this.props.pageSize);
+    },
     render: function(){
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.props.products
-                            .filter(this.productMatchesSearch)
-                            .map(function(product){
-                                return <ProductRow key={product.id} product={product}/>
-                            })
-                    }
-                </tbody>
-            </table>
+            <div className="productTable">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.products
+                                .filter(this.productMatchesSearch)
+                                .map(function(product){
+                                    return <ProductRow key={product.id} product={product}/>
+                                })
+                        }
+                    </tbody>
+                </table>
+                <Pager numberOfPages={this.calculateAmountOfPages()} />
+            </div>
         )
     }
 });
@@ -44,7 +79,7 @@ var SearchBar = React.createClass({
     },
     render: function(){
         return (
-            <form>
+            <form className="searchBar">
                 <input type="text"
                        ref="filterTextInput"
                        placeholder="Search..."
@@ -58,21 +93,25 @@ var SearchBar = React.createClass({
 });
 
 var FilterableProductTable = React.createClass({
-    handleUserInput:function(filterText){
+    handleSearch:function(filterText){
         this.setState({
             filterText: filterText
         });
     },
     getInitialState: function(){
         return {
-            filterText: ''
+            filterText: '',
+            currentPage:0
         };
     },
     render: function(){
         return (
             <div>
-                <SearchBar filterText={this.state.filterText} inputCallback={this.handleUserInput}/>
-                <ProductTable products={this.props.products} filterText={this.state.filterText}/>
+                <SearchBar filterText={this.state.filterText} inputCallback={this.handleSearch}/>
+                <ProductTable products={this.props.products}
+                              filterText={this.state.filterText}
+                              pageSize="9"
+                />
             </div>
         );
     }
