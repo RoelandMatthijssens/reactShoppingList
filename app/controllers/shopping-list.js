26f84c89da45f8ace462be78245628ae2b56ad46
Roeland <roeland.matthijssens@vikingco.com>
2018-03-12 23:50:23 +0100

    Add shoppingListItem relation

--- app/controllers/shopping-list.js
+++ app/controllers/shopping-list.js
@@ -4,3 +4,21 @@ const restify = require('../utils/restify');
 const Models = require('../models');
 
 Object.assign(exports, restify.restify(Models.ShoppingList));
+
+exports.showItems = (req, res) => {
+    Models.ShoppingList
+        .findById(req.params.id)
+        .then(list => list.getItems())
+        .then(items => res.json(items))
+        .catch(err => res.status(400).send(err));
+}
+
+exports.addItem = (req, res) => {
+    Promise.all([
+        Models.ShoppingList.findById(req.params.id),
+        Models.Item.findById(req.body.itemId)
+    ])
+    .then(([list, item]) => {list.addItem(item); return item})
+    .then(item => res.json(item))
+    .catch(err => res.status(400).send(err));
+}
---
+++ app/models/shopping-list-item.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+    const ShoppingListItem = sequelize.define("ShoppingListItem", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+            validate: {min: 0}
+        }
+    });
+
+    ShoppingListItem.associate = models => {
+        ShoppingListItem.belongsTo(models.Item, { foreignKey: 'itemId', targetKey: 'id' })
+        ShoppingListItem.belongsTo(models.ShoppingList, { foreignKey: 'shoppingListId', targetKey: 'id' })
+    }
+
+    return ShoppingListItem
+};
