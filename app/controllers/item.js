f651e7010ab2aa836aba0aa26c7b123203e9cffd
Jeroen Matthijssens <jeroen.matthijssens@gmail.com>
2018-03-12 09:31:26 +0100

    Remove duplication by using restify utility.

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -1,45 +1,6 @@
 'use strict';
 
+const restify = require('../utils/restify');
 const Models = require('../models');
 
-exports.list = function(req, res) {
-    Models.Item
-        .findAll()
-        .then(items => res.json(items));
-};
-
-exports.create = function(req, res) {
-    Models.Item
-        .build(req.body)
-        .save()
-        .then(newItem => res.json(newItem))
-        .catch(err => res.status(400).send(err));
-};
-
-exports.view = function(req, res) {
-    let query = {id: req.params.id}
-    Models.Item
-        .findOne({where: query})
-        .then(item => res.json(item))
-        .catch(err => res.status(400).send(err));
-};
-
-exports.update = function(req, res){
-    Models.Item
-        .findById(req.params.id)
-        .then(item => {
-            item.updateAttributes(req.body);
-            res.json(item);
-        })
-        .catch(err => res.status(400).send(err));
-};
-
-exports.destroy = function(req, res){
-    Models.Item
-        .findById(req.params.id)
-        .then(item => {
-            item.destroy();
-            res.json(item);
-        })
-        .catch(err => res.status(400).send(err));
-};
+Object.assign(exports, restify.restify(Models.Item));
