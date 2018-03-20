a40240727e2023479fdefb40bc2499309595e9c5
Jeroen Matthijssens <jeroen.matthijssens@gmail.com>
2018-03-20 10:31:00 +0100

    Make the build directory relative to the repo base.

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 	},
 	output: {
 		filename: 'js/[name].js',
-		path: path.resolve (__dirname, 'build/dist'),
+		path: path.resolve ('./build/dist'),
 		publicPath: isProd ? '/' : 'http://localhost:3000/'
 	},
 	module: {
